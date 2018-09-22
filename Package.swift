537f3960b3b9e50b38cc56d95b1b0683acb47042
Jan-Hendrik Matthes <matthesjh@gmail.com>
2018-09-22 12:55:14 +0200

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version:4.2
+
+import PackageDescription
+
+let package = Package(
+    name: "sc19-swift-client",
+    dependencies: [],
+    targets: [
+        .target(name: "simple-client", dependencies: [])
+    ],
+    swiftLanguageVersions: [.v4_2]
+)
---
+++ README.md
@@ -0,0 +1,3 @@
+# Swift client for the Software-Challenge Germany 2018/2019
+
+This package contains a simple client written in the [Swift Programming Language](https://swift.org/) for the [Software-Challenge Germany](https://www.software-challenge.de/) 2018/2019.
