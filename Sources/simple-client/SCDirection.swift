e89295f68edbcf76d7234ce92c5da5f06f1eb5d9
Jan-Hendrik Matthes <matthesjh@gmail.com>
2018-09-23 17:49:03 +0200

    Add an enum for the direction of a move

---
+++ Sources/simple-client/SCDirection.swift
@@ -0,0 +1,23 @@
+/// The direction of a move.
+enum SCDirection: String, CustomStringConvertible {
+    /// Move to north.
+    case up = "UP"
+    /// Move to northeast.
+    case upRight = "UP_RIGHT"
+    /// Move to east.
+    case right = "RIGHT"
+    /// Move to southeast.
+    case downRight = "DOWN_RIGHT"
+    /// Move to south.
+    case down = "DOWN"
+    /// Move to southwest.
+    case downLeft = "DOWN_LEFT"
+    /// Move to west.
+    case left = "LEFT"
+    /// Move to northwest.
+    case upLeft = "UP_LEFT"
+
+    var description: String {
+        return self.rawValue
+    }
+}
