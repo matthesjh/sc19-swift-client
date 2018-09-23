7d5b5b1f93a3774aa7eae672e05ce0349ecb43df
Jan-Hendrik Matthes <matthesjh@gmail.com>
2018-09-23 20:29:35 +0200

    Improve the doc comments of the field state enum

--- Sources/simple-client/SCFieldState.swift
+++ Sources/simple-client/SCFieldState.swift
@@ -1,8 +1,8 @@
 /// The state of a field.
 enum SCFieldState: String, CustomStringConvertible {
-    /// The field is covered with a red fish.
+    /// The field is covered with a red piranha.
     case red = "RED"
-    /// The field is covered with a blue fish.
+    /// The field is covered with a blue piranha.
     case blue = "BLUE"
     /// The field is covered with an octopus.
     case obstructed = "OBSTRUCTED"
