cd0af70e572d2ead563ca36802fb50f5181d735c
Jan-Hendrik Matthes <matthesjh@gmail.com>
2018-09-25 19:28:44 +0200

    Add constant for the maximum number of piranhas per player

--- Sources/simple-client/SCConstants.swift
+++ Sources/simple-client/SCConstants.swift
@@ -2,6 +2,8 @@
 struct SCConstants {
     /// The size of the game board.
     static let boardSize = 10
+    /// The maximum number of piranhas per player.
+    static let maxNumberOfPiranhas = (boardSize - 2) * 2
     /// The maximum number of rounds per game.
     static let roundLimit = 30
     /// The maximum number of turns per game.
