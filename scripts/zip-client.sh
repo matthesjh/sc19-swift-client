27c402cea003cd9a5fc00b41824242b4c0ff3e6c
Jan-Hendrik Matthes <matthesjh@gmail.com>
2018-10-04 19:04:30 +0200

    Improve Travis build script

--- scripts/zip-client.sh
+++ scripts/zip-client.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
+BUILD_CONFIG=release
 EXECUTABLE_NAME=simple-client
 
 echo "Using Swift version:"
 swift -version
 
 echo "Building package..."
-swift build -c release -Xswiftc -O
+swift build -c "${BUILD_CONFIG}" -Xswiftc -O
 
 echo "Zipping files..."
-zip -j "${EXECUTABLE_NAME}".zip scripts/start-client.sh .build/release/"${EXECUTABLE_NAME}"
+zip -j "${EXECUTABLE_NAME}".zip scripts/start-client.sh .build/"${BUILD_CONFIG}"/"${EXECUTABLE_NAME}"
