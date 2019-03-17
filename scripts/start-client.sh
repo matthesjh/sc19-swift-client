2dd495bd71a2999da1b024dca57d7b7ad59479e5
Jan-Hendrik Matthes <matthesjh@gmail.com>
2019-03-17 14:08:12 +0100

    Adjust the start script to show the installed Swift version

--- scripts/start-client.sh
+++ scripts/start-client.sh
@@ -4,5 +4,8 @@ EXECUTABLE_NAME=simple-client
 
 export LD_LIBRARY_PATH=/usr/lib/swift/linux:"${LD_LIBRARY_PATH}"
 
+echo "Using Swift version:"
+swift -version
+
 chmod u+x "${EXECUTABLE_NAME}"
 ./"${EXECUTABLE_NAME}" "$@"
