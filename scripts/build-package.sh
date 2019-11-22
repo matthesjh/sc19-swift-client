6c529f63152821750337813759db6c27a18d0da9
Jan-Hendrik Matthes <matthesjh@gmail.com>
2019-11-22 15:30:09 +0100

    Enable Linux builds on GitHub Actions

--- scripts/build-package.sh
+++ scripts/build-package.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
+if [ "${TRAVIS_OS_NAME}" = "linux" ] || [ "${RUNNER_OS}" = "Linux" ]; then
   SWIFT_VERSION_NUMBER=4.2.4
-  SWIFT_PLATFORM=ubuntu16.04
+  SWIFT_PLATFORM=ubuntu$(lsb_release -r -s)
   SWIFT_BRANCH=swift-${SWIFT_VERSION_NUMBER}-release
   SWIFT_VERSION=swift-${SWIFT_VERSION_NUMBER}-RELEASE
 
   echo "Downloading the Swift ${SWIFT_VERSION_NUMBER} toolchain..."
   wget https://swift.org/builds/${SWIFT_BRANCH}/$(echo ${SWIFT_PLATFORM} | tr -d .)/${SWIFT_VERSION}/${SWIFT_VERSION}-${SWIFT_PLATFORM}.tar.gz
   tar xzf ${SWIFT_VERSION}-${SWIFT_PLATFORM}.tar.gz
-  export PATH="${TRAVIS_BUILD_DIR}"/${SWIFT_VERSION}-${SWIFT_PLATFORM}/usr/bin:"${PATH}"
+  export PATH="${TRAVIS_BUILD_DIR:-${GITHUB_WORKSPACE}}"/${SWIFT_VERSION}-${SWIFT_PLATFORM}/usr/bin:"${PATH}"
 fi
 
 echo "Using Swift version:"
